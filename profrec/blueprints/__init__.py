8e288cbcb3c43b27d57fe552f696a689b87c63af
vedarth31 <vedarth31@gmail.com>
2024-02-04 21:53:29 -0600

    fix cors attempt

--- profrec/blueprints/__init__.py
+++ profrec/blueprints/__init__.py
@@ -4,7 +4,6 @@ from .api import apis
 from flask_cors import CORS
 
 app = Flask(__name__)
-from flask_cors import CORS
 
 CORS(app, resources={r"/api/*": {"origins": "https://prof-rec.vercel.app"}}, supports_credentials=True, allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin"], methods=["GET", "POST", "OPTIONS"])
 
