f80c4af476848207999f8518b6d66668a6be0b93
vedarth31 <vedarth31@gmail.com>
2024-02-04 21:48:55 -0600

    cors edit

--- profrec/blueprints/__init__.py
+++ profrec/blueprints/__init__.py
@@ -6,6 +6,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 from flask_cors import CORS
 
-CORS(app, resources={r"/api/*": {"origins": "https://prof-rec.vercel.app"}}, supports_credentials=True, allow_headers=["Content-Type", "Authorization"], methods=["GET", "POST", "OPTIONS"])
+CORS(app, resources={r"/api/*": {"origins": "https://prof-rec.vercel.app"}}, supports_credentials=True, allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin"], methods=["GET", "POST", "OPTIONS"])
 
 app.register_blueprint(apis)
